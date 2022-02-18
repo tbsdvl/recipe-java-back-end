ffb25a4ce14a4f34b685063334ef4f14c2f53f6f
Triston Burns <tristonburns97@gmail.com>
2022-02-18 14:49:36 -0700

    Update GET '/recipes/ method in recipes controller

--- src/main/java/com/example/recipe/controller/RecipeController.java
+++ src/main/java/com/example/recipe/controller/RecipeController.java
@@ -1,12 +1,24 @@
 package com.example.recipe.controller;
 
+import com.example.recipe.model.Recipe;
 import com.example.recipe.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RecipeController {
     @Autowired
     RecipeRepository recipeRepo;
+
+    @GetMapping("/recipes")
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Recipe> getRecipes() {
+        return recipeRepo.findAll();
+    }
 
 }
